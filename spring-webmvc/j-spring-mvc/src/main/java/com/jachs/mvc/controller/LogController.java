b44ce2a5e1d4cad67f8bdde719db6881aff46da3
jachs <799516423@qq.com>
2021-11-19 18:00:20 +0800

    'add_mvc_temp'

---
+++ spring-webmvc/j-spring-mvc/src/main/java/com/jachs/mvc/controller/LogController.java
@@ -0,0 +1,20 @@
+package com.jachs.mvc.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LogController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request,
+                                      HttpServletResponse response) throws Exception {
+        ModelAndView modelView = new ModelAndView();
+        //类似于 request.setAttribute()
+        modelView.addObject("name","张三");
+        modelView.setViewName("/WEB-INF/view/index.jsp");
+        return modelView;
+    }
+
+}
---
+++ spring-webmvc/j-spring-mvc/src/main/webapp/WEB-INF/view/index.jsp
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<%@ page language="java" contentType="text/html; charset=GBK"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<body>
+<h2>Hello World!</h2>
+<%${name}%>
+</body>
+</html>
