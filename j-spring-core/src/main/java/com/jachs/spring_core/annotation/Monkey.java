d45205012adc7dc694ce843d53d225495629ae45
jachs <799516423@qq.com>
2021-12-01 14:48:51 +0800

    添加初始化销毁

--- j-spring-core/src/main/java/com/jachs/spring_core/annotation/Monkey.java
+++ j-spring-core/src/main/java/com/jachs/spring_core/annotation/Monkey.java
@@ -1,16 +1,26 @@
 package com.jachs.spring_core.annotation;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
 /**
  * 
  * @author zhanchaohan
  *
  */
-public class Monkey {
-    
+public class Monkey implements DisposableBean , InitializingBean {
     private String monkeyName = "吉吉";
-    
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("销毁对象Monkey");
+    }
     public String toString(){
         return "MonkeyName:" + monkeyName;
     }
-
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("初始化对象Monkey");
+        this.monkeyName="毛毛";
+    }
 }
--- j-spring-core/src/test/java/com/jachs/spring_core/annotation/AnnotationTest.java
+++ j-spring-core/src/test/java/com/jachs/spring_core/annotation/AnnotationTest.java
@@ -1,7 +1,7 @@
 package com.jachs.spring_core.annotation;
 
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -13,10 +13,12 @@ public class AnnotationTest {
 	
 	@Test
 	public void test1() {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("Annotation.xml");
+		AbstractApplicationContext ctx = new ClassPathXmlApplicationContext("Annotation.xml");
 		Zoo zoo = ctx.getBean("zoo", Zoo.class);
 
 		zoo.printTiger();
 		zoo.printMonkey();
+
+		ctx.registerShutdownHook();//关闭钩子触发destory
 	}
 }
